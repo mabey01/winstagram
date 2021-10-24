1a826ccaf05a8d68f83f6a6e4893e6337442b6cb
Maximilian Beyer <mabey01@gmail.com>
2021-10-24 15:00:10 +1000

    add hero image

--- src/Winstagram.tsx
+++ src/Winstagram.tsx
@@ -5,7 +5,7 @@ export function Winstagram() {
   return (
     <div>
       <header className="bg-white w-full">
-        <div className="max-w-5xl m-auto py-4 flex justify-between items-center">
+        <div className="max-w-5xl m-auto py-4 px-4 flex justify-between items-center">
           <div className="flex items-baseline">
             <h1 className="font-mono font-semibold">Winstagram</h1>
             <span className="text-xs text-gray-300 ml-2">by Max</span>
@@ -18,28 +18,60 @@ export function Winstagram() {
         </div>
       </header>
 
-      <section className="max-w-5xl m-auto mt-16">
-        <ProfileHeader
-          profileName="profile name"
-          profileImage="https://source.unsplash.com/200x200/?portrait"
-          numberOfPosts="124"
-          numberOfFollowers="12.4m"
-          numberOfFollowing="1234"
-          name="human name"
-          pronouns="she/her"
-          bio="Photographer"
-        />
-        <div className="mt-16 border-t border-gray-300">
-          <ul className="flex justify-center space-x-16 py-4 uppercase text-gray-500 text-sm cursor-pointer font-medium">
-            <li className="text-black">Posts</li>
-            <li>Reels</li>
-            <li>Videos</li>
-            <li>Tagged</li>
-          </ul>
+      <section className="">
+        <div id="hero">
+          <picture>
+            <source
+              media="(orientation: landscape)"
+              srcSet="https://source.unsplash.com/2000x500/?landscape"
+              type="image/jpeg"
+            />
+            <source
+              media="(orientation: portrait)"
+              srcSet="https://source.unsplash.com/1000x1000/?landscape"
+              type="image/jpeg"
+            />
+            <img
+              className="w-full h-72 bg-gray-300 object-cover"
+              src="https://source.unsplash.com/2000x500/?landscape"
+              alt="Hero image"
+            />
+          </picture>
         </div>
+        <div className="max-w-5xl m-auto px-4">
+          <div>
+            <header className="flex flex-col items-center sm:flex-row">
+              <div className="-mt-40">
+                <img
+                  src="https://source.unsplash.com/200x200/?portrait"
+                  className="rounded-full w-40 h-40 bg-gray-400 border-4 border-gray-200"
+                />
+              </div>
+              <div className="mt-4 ml-16">
+                <ProfileHeader
+                  profileName="profile name"
+                  numberOfPosts="124"
+                  numberOfFollowers="12.4m"
+                  numberOfFollowing="1234"
+                  name="human name"
+                  pronouns="she/her"
+                  bio="Photographer"
+                />
+              </div>
+            </header>
+          </div>
+          <div className="mt-8 sm:mt-16 border-t border-gray-300">
+            <ul className="flex justify-center space-x-8 sm:space-x-16 py-4 uppercase text-gray-500 text-sm cursor-pointer font-medium">
+              <li className="text-black">Posts</li>
+              <li>Reels</li>
+              <li>Videos</li>
+              <li>Tagged</li>
+            </ul>
+          </div>
 
-        <div>
-          <ImageGrid />
+          <div>
+            <ImageGrid />
+          </div>
         </div>
       </section>
 
--- src/components/profile-header.tsx
+++ src/components/profile-header.tsx
@@ -1,6 +1,5 @@
 interface ProfileHeaderProps {
   profileName: string;
-  profileImage: string;
   numberOfPosts: React.ReactNode;
   numberOfFollowers: React.ReactNode;
   numberOfFollowing: React.ReactNode;
@@ -11,7 +10,6 @@ interface ProfileHeaderProps {
 
 export function ProfileHeader({
   profileName,
-  profileImage,
   numberOfPosts,
   numberOfFollowers,
   numberOfFollowing,
@@ -20,50 +18,46 @@ export function ProfileHeader({
   bio,
 }: ProfileHeaderProps) {
   return (
-    <div className="flex">
-      <img src={profileImage} className="rounded-full w-40" />
-
-      <div className="ml-20">
-        <div className="flex items-center">
-          <h3 className="text-3xl font-light">{profileName}</h3>
-          <div className="ml-2 text-blue-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-
-          <button className="ml-6 bg-blue-500 py-1.5 px-2 rounded text-white font-semibold text-sm">
-            Follow
-          </button>
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center">
+        <h3 className="text-3xl font-light">{profileName}</h3>
+        <div className="ml-2 text-blue-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+              clipRule="evenodd"
+            />
+          </svg>
         </div>
-        <ul className="flex space-x-4 mt-4">
-          <li>
-            <span className="font-semibold">{numberOfPosts}</span> posts
-          </li>
-          <li>
-            <span className="font-semibold">{numberOfFollowers}</span> followers
-          </li>
-          <li>
-            <span className="font-semibold">{numberOfFollowing}</span> following
-          </li>
-        </ul>
 
-        <div className="mt-4">
-          <div className="flex">
-            <div className="font-semibold">{name}</div>
-            <div className="ml-2 text-gray-400">{pronouns}</div>
-          </div>
-          <div className="">{bio}</div>
+        <button className="ml-6 bg-blue-500 py-1.5 px-2 rounded text-white font-semibold text-sm">
+          Follow
+        </button>
+      </div>
+      <ul className="flex space-x-4 mt-4">
+        <li>
+          <span className="font-semibold">{numberOfPosts}</span> posts
+        </li>
+        <li>
+          <span className="font-semibold">{numberOfFollowers}</span> followers
+        </li>
+        <li>
+          <span className="font-semibold">{numberOfFollowing}</span> following
+        </li>
+      </ul>
+
+      <div className="mt-4">
+        <div className="flex">
+          <div className="font-semibold">{name}</div>
+          <div className="ml-2 text-gray-400">{pronouns}</div>
         </div>
+        <div className="">{bio}</div>
       </div>
     </div>
   );
